6c3939e083a820a86d685a18afa3c570590ad5e8
TRAMPlin30 <vicodin28@gmail.com>
2021-05-31 09:51:43 +0300

    heroku - 4

--- src/train_rout/settings/production.py
+++ src/train_rout/settings/production.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent   # BASE_DIR - обье
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'g56=0pj=-c%be+r^ok9+v#plw%6b3lbjdx06q#we*il&@+2wkd'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
